c887509abaaecaf617414f118829949a851de1b0
Ana Pacheco <kornetera@gmail.com>
2023-03-09 20:45:47 +0000

    apuntes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-
+const URL = "https://assets.breatheco.de/apis/fake/todos/user/apacheco"
 
 const Home = () => {
+
+	const [tarea, setTarea] = useState()
+
+	useEffect(()=>{
+fetch(URL, {method: "GET", headers:{"Content-Type":"application/json"}})
+.then((response) => {
+	console.log("response entero", response)
+	console.log("ok del servidor",response.ok)
+	console.log("status de response",response.status)
+	console.log("texto response",response.text)
+	return response.json()
+})
+.then((data) => {
+	console.log("la data", data)
+	return setTarea(data)
+
+}) 
+.catch((error)=>{
+	console.log("el error",error)
+})
+	},[])
+
+
 	return (
 		<div className="text-center">
-		
+		hola
 	
 		</div>
 	);
